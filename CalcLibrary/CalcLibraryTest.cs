5cec11edd9d7ae2d02b7bd9dc29062c4e42fc7b8
Riccardo Rossi <riccardorossi.r2004@outlook.it>
2020-01-04 11:07:28 +0100

    Add project files.

---
+++ CalcLibrary/CalcLibraryTest.cs
@@ -0,0 +1,35 @@
+using Calcoli;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcLibrary
+{
+    [TestClass]
+    public class CalcLibraryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double rad = Calc.RadQ(12);
+            Assert.AreEqual(rad, 2);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double rad = Calc.RadQ(-7);
+            Assert.AreEqual(rad, double.NaN);
+       
+        }
+        public void TestMethod3()
+        {
+            double rad = Calc.RadQ(8);
+            Assert.AreEqual(rad, 2);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            double rad = Calc.RadQ(-9);
+            Assert.AreEqual(rad, double.NaN);
+
+        }
+    }
+}
---
+++ WPFCalcoloLibreria/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Calcoli;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCalcoloLibreria
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnCalcola_Click(object sender, RoutedEventArgs e)
+        {
+            double num = double.Parse(txtNumero.Text);
+            double radq = Calc.RadQ(num);
+            LblRisultato.Content = (radq);
+        }
+    }
+}
+    
